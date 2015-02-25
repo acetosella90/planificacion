701b6c784629e65b4c8afd072556a684e27bd73e
Rama <r_xifro@hotmail.com>
2015-02-25 15:36:49 -0300

    commit 3

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ if ($_SESSION['usuario'] == "rutita" && $_SESSION['pass'] == "cadabra") {
             <meta name="description" content="">
             <meta name="author" content="">
             <link rel="icon" href="../../favicon.ico">
-
+            
             <title>Dashboard Template for Bootstrap</title>
 
             <!-- Bootstrap core CSS -->
@@ -25,7 +25,7 @@ if ($_SESSION['usuario'] == "rutita" && $_SESSION['pass'] == "cadabra") {
             <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
             <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
             <script src="js/ie-emulation-modes-warning.js"></script>
-
+            
             <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
             <!--[if lt IE 9]>
               <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
@@ -61,9 +61,9 @@ if ($_SESSION['usuario'] == "rutita" && $_SESSION['pass'] == "cadabra") {
             </nav>
 
             <div class="container-fluid">
-
+                 
                 <div class="row">
-                    <div class="col-sm-3 col-md-2 sidebar">
+                    <div  class="col-sm-3 col-md-2 sidebar">
                         <ul class="nav nav-sidebar">
                             <li class="active"><a href="#">Overview <span class="sr-only">(current)</span></a></li>
                             <li><a href="#">Reports</a></li>
@@ -81,19 +81,20 @@ if ($_SESSION['usuario'] == "rutita" && $_SESSION['pass'] == "cadabra") {
                     <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
                         <h1 class="page-header">Unidades de analisis</h1>
                         <div class="row placeholders">
-                            <input type="button" onclick=""/> <div class="col-xs-6 col-sm-4 placeholder">
-                                <img data-src="holder.js/200x200/auto/sky" src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iMjAwIiBoZWlnaHQ9IjIwMCIgdmlld0JveD0iMCAwIDIwMCAyMDAiIHByZXNlcnZlQXNwZWN0UmF0aW89Im5vbmUiPjxkZWZzPjwvZGVmcz48cmVjdCB3aWR0aD0iMjAwIiBoZWlnaHQ9IjIwMCIgZmlsbD0iIzBEOEZEQiI+PC9yZWN0PjxnPjx0ZXh0IHg9Ijc1LjUiIHk9IjEwMCIgc3R5bGU9ImZpbGw6I0ZGRkZGRjtmb250LXdlaWdodDpib2xkO2ZvbnQtZmFtaWx5OkFyaWFsLCBIZWx2ZXRpY2EsIE9wZW4gU2Fucywgc2Fucy1zZXJpZiwgbW9ub3NwYWNlO2ZvbnQtc2l6ZToxMHB0O2RvbWluYW50LWJhc2VsaW5lOmNlbnRyYWwiPjIwMHgyMDA8L3RleHQ+PC9nPjwvc3ZnPg==" class="img-responsive" alt="Generic placeholder thumbnail">
-                                <h4>Mapuche y Rhun</h4>
+                             <div id='mapuche' class="col-xs-6 col-sm-4 placeholder">
+                                  <img data-src="holder.js/200x200/auto/sky" src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iMjAwIiBoZWlnaHQ9IjIwMCIgdmlld0JveD0iMCAwIDIwMCAyMDAiIHByZXNlcnZlQXNwZWN0UmF0aW89Im5vbmUiPjxkZWZzPjwvZGVmcz48cmVjdCB3aWR0aD0iMjAwIiBoZWlnaHQ9IjIwMCIgZmlsbD0iIzBEOEZEQiI+PC9yZWN0PjxnPjx0ZXh0IHg9Ijc1LjUiIHk9IjEwMCIgc3R5bGU9ImZpbGw6I0ZGRkZGRjtmb250LXdlaWdodDpib2xkO2ZvbnQtZmFtaWx5OkFyaWFsLCBIZWx2ZXRpY2EsIE9wZW4gU2Fucywgc2Fucy1zZXJpZiwgbW9ub3NwYWNlO2ZvbnQtc2l6ZToxMHB0O2RvbWluYW50LWJhc2VsaW5lOmNlbnRyYWwiPjIwMHgyMDA8L3RleHQ+PC9nPjwvc3ZnPg==" class="img-circle" alt="Generic placeholder thumbnail"> 
+                                
+                                 <h4>Mapuche y Rhun</h4>
                                 <span class="text-muted">Personal Planta</span>
                             </div>
-                            <div class="col-xs-6 col-sm-4 placeholder">
+                            <div id="pilaga" class="col-xs-6 col-sm-4 placeholder" >
                                 <img data-src="holder.js/200x200/auto/sky" src= "data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iMjAwIiBoZWlnaHQ9IjIwMCIgdmlld0JveD0iMCAwIDIwMCAyMDAiIHByZXNlcnZlQXNwZWN0UmF0aW89Im5vbmUiPjxkZWZzPjwvZGVmcz48cmVjdCB3aWR0aD0iMjAwIiBoZWlnaHQ9IjIwMCIgZmlsbD0iIzBEOEZEQiI+PC9yZWN0PjxnPjx0ZXh0IHg9Ijc1LjUiIHk9IjEwMCIgc3R5bGU9ImZpbGw6I0ZGRkZGRjtmb250LXdlaWdodDpib2xkO2ZvbnQtZmFtaWx5OkFyaWFsLCBIZWx2ZXRpY2EsIE9wZW4gU2Fucywgc2Fucy1zZXJpZiwgbW9ub3NwYWNlO2ZvbnQtc2l6ZToxMHB0O2RvbWluYW50LWJhc2VsaW5lOmNlbnRyYWwiPjIwMHgyMDA8L3RleHQ+PC9nPjwvc3ZnPg=="
                                 class="img-responsive" alt="Generic placeholder thumbnail">
                                  <h4>Pilaga</h4>
 								<span class="text-muted">Presupuestaria</span>
                            
 							</div>
-                            <div class="col-xs-6 col-sm-4 placeholder">
+                            <div  id="araucano" class="col-xs-6 col-sm-4 placeholder">
                                 <img data-src="holder.js/200x200/auto/sky" src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iMjAwIiBoZWlnaHQ9IjIwMCIgdmlld0JveD0iMCAwIDIwMCAyMDAiIHByZXNlcnZlQXNwZWN0UmF0aW89Im5vbmUiPjxkZWZzPjwvZGVmcz48cmVjdCB3aWR0aD0iMjAwIiBoZWlnaHQ9IjIwMCIgZmlsbD0iIzBEOEZEQiI+PC9yZWN0PjxnPjx0ZXh0IHg9Ijc1LjUiIHk9IjEwMCIgc3R5bGU9ImZpbGw6I0ZGRkZGRjtmb250LXdlaWdodDpib2xkO2ZvbnQtZmFtaWx5OkFyaWFsLCBIZWx2ZXRpY2EsIE9wZW4gU2Fucywgc2Fucy1zZXJpZiwgbW9ub3NwYWNlO2ZvbnQtc2l6ZToxMHB0O2RvbWluYW50LWJhc2VsaW5lOmNlbnRyYWwiPjIwMHgyMDA8L3RleHQ+PC9nPjwvc3ZnPg=="  class="img-responsive" alt="Generic placeholder thumbnail"> 
                                 <h4>Araucano y Guarani</h4>
                                 <span class="text-muted">Academica alumnos</span>
@@ -103,134 +104,118 @@ if ($_SESSION['usuario'] == "rutita" && $_SESSION['pass'] == "cadabra") {
 						
 						
 						<h2 class="sub-header">Section title</h2>
-                        <div class="table-responsive">
+                        <div  id="tabla1" class="table-responsive tabla" style="display:none;">
                             <table class="table table-striped">
                                 <thead>
                                     <tr>
                                         <th>#</th>
-                                        <th>Header</th>
-                                        <th>Header</th>
-                                        <th>Header</th>
+                                        <th>Reportes</th>
+                                        <th>Descripcion</th>
+                                        <th>Tipo de grafico</th>
                                         <th>Header</th>
                                     </tr>
                                 </thead>
                                 <tbody>
                                     <tr>
-                                        <td>1,001</td>
-                                        <td>Lorem</td>
+                                        <td>1</td>
+                                        <td>mapuche</td>
                                         <td>ipsum</td>
                                         <td>dolor</td>
                                         <td>sit</td>
                                     </tr>
                                     <tr>
-                                        <td>1,002</td>
+                                        <td>2</td>
                                         <td>amet</td>
                                         <td>consectetur</td>
                                         <td>adipiscing</td>
                                         <td>elit</td>
                                     </tr>
                                     <tr>
-                                        <td>1,003</td>
+                                        <td>3</td>
                                         <td>Integer</td>
                                         <td>nec</td>
                                         <td>odio</td>
                                         <td>Praesent</td>
                                     </tr>
-                                    <tr>
-                                        <td>1,003</td>
-                                        <td>libero</td>
-                                        <td>Sed</td>
-                                        <td>cursus</td>
-                                        <td>ante</td>
-                                    </tr>
-                                    <tr>
-                                        <td>1,004</td>
-                                        <td>dapibus</td>
-                                        <td>diam</td>
-                                        <td>Sed</td>
-                                        <td>nisi</td>
-                                    </tr>
-                                    <tr>
-                                        <td>1,005</td>
-                                        <td>Nulla</td>
-                                        <td>quis</td>
-                                        <td>sem</td>
-                                        <td>at</td>
-                                    </tr>
-                                    <tr>
-                                        <td>1,006</td>
-                                        <td>nibh</td>
-                                        <td>elementum</td>
-                                        <td>imperdiet</td>
-                                        <td>Duis</td>
-                                    </tr>
-                                    <tr>
-                                        <td>1,007</td>
-                                        <td>sagittis</td>
-                                        <td>ipsum</td>
-                                        <td>Praesent</td>
-                                        <td>mauris</td>
-                                    </tr>
-                                    <tr>
-                                        <td>1,008</td>
-                                        <td>Fusce</td>
-                                        <td>nec</td>
-                                        <td>tellus</td>
-                                        <td>sed</td>
-                                    </tr>
-                                    <tr>
-                                        <td>1,009</td>
-                                        <td>augue</td>
-                                        <td>semper</td>
-                                        <td>porta</td>
-                                        <td>Mauris</td>
-                                    </tr>
-                                    <tr>
-                                        <td>1,010</td>
-                                        <td>massa</td>
-                                        <td>Vestibulum</td>
-                                        <td>lacinia</td>
-                                        <td>arcu</td>
-                                    </tr>
-                                    <tr>
-                                        <td>1,011</td>
-                                        <td>eget</td>
-                                        <td>nulla</td>
-                                        <td>Class</td>
-                                        <td>aptent</td>
-                                    </tr>
-                                    <tr>
-                                        <td>1,012</td>
-                                        <td>taciti</td>
-                                        <td>sociosqu</td>
-                                        <td>ad</td>
-                                        <td>litora</td>
-                                    </tr>
-                                    <tr>
-                                        <td>1,013</td>
-                                        <td>torquent</td>
-                                        <td>per</td>
-                                        <td>conubia</td>
-                                        <td>nostra</td>
-                                    </tr>
-                                    <tr>
-                                        <td>1,014</td>
-                                        <td>per</td>
-                                        <td>inceptos</td>
-                                        <td>himenaeos</td>
-                                        <td>Curabitur</td>
-                                    </tr>
-                                    <tr>
-                                        <td>1,015</td>
-                                        <td>sodales</td>
-                                        <td>ligula</td>
-                                        <td>in</td>
-                                        <td>libero</td>
-                                    </tr>
+                               
                                 </tbody>
                             </table>
                         </div>
-                    </div>
+                    <div id="tabla2" class="table-responsive" style="display:none;">
+                            <table class="table table-striped">
+                                <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Reportes</th>
+                                        <th>Descripcion</th>
+                                        <th>Tipo de grafico</th>
+                                        <th>Header</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>pilaga</td>
+                                        <td>ipsum</td>
+                                        <td>dolor</td>
+                                        <td>sit</td>
+                                    </tr>
+                                    <tr>
+                                        <td>2</td>
+                                        <td>amet</td>
+                                        <td>consectetur</td>
+                                        <td>adipiscing</td>
+                                        <td>elit</td>
+                                    </tr>
+                                    <tr>
+                                        <td>3</td>
+                                        <td>Integer</td>
+                                        <td>nec</td>
+                                        <td>odio</td>
+                                        <td>Praesent</td>
+                                    </tr>
+                               
+                                </tbody>
+                            </table>
+                        </div>
+                         <div  id="tabla3" class="table-responsive" style="display:none;">
+                            <table class="table table-striped">
+                                <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Reportes</th>
+                                        <th>Descripcion</th>
+                                        <th>Tipo de grafico</th>
+                                        <th>Header</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>araucano y guarani</td>
+                                        <td>ipsum</td>
+                                        <td>dolor</td>
+                                        <td>sit</td>
+                                    </tr>
+                                    <tr>
+                                        <td>2</td>
+                                        <td>amet</td>
+                                        <td>consectetur</td>
+                                        <td>adipiscing</td>
+                                        <td>elit</td>
+                                    </tr>
+                                    <tr>
+                                        <td>3</td>
+                                        <td>Integer</td>
+                                        <td>nec</td>
+                                        <td>odio</td>
+                                        <td>Praesent</td>
+                                    </tr>
+                               
+                                </tbody>
+                            </table>
+                        </div>
+                        </div>
                 </div>
             </div>
 
@@ -240,8 +225,9 @@ if ($_SESSION['usuario'] == "rutita" && $_SESSION['pass'] == "cadabra") {
             <script src="js/jquery.min.js"></script>
             <script src="js/bootstrap.min.js"></script>
             <script src="js/doc.min.js"></script>
-
-        </body>
+ <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+  <script src='js/botones.js'></script>
+            </body>
     </html>
 <?php
 } else {
