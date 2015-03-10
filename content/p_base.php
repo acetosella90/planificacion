204224e112e74979f0bf68297a6b89d6c49b3d69
Mati <acetosella90@gmail.com>
2015-03-10 15:06:14 -0300

    ajax en carga de paginas

---
+++ content/p_base.php
@@ -0,0 +1,164 @@
+<div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+    <h1 class="page-header">Unidades de analisis</h1>
+    <div class="row ">
+        <div id='mapuche' class="col-xs-6 col-sm-4  " >
+            <img  src= "img/siu-mapuche.gif" class="img-responsive" style="width: 200px; margin-bottom: 35px;">
+            <h4>Mapuche y Rhun</h4> 
+            <span class="text-muted">Personal Planta</span>
+        </div>
+        <div id="pilaga" class="col-xs-6 col-sm-4 " >
+            <img src="img/siu-pilaga.jpg"  class="img-responsive" style="width: 240px; margin-bottom: 30px;">
+            <h4>Pilaga</h4>
+            <span class="text-muted">Presupuestaria</span>
+        </div>
+        <div  id="araucano" class="col-xs-6 col-sm-4 ">
+            <img src="img/siu-guarani.jpg" class="img-responsive" style="width: 140px;"> 
+            <img src="img/siu-araucano.png" class="img-responsive" style="width: 1500px;"> 
+            <h4>Araucano y Guarani</h4>
+            <span class="text-muted">Academica alumnos</span>
+        </div>
+        <h2 class="sub-header">Section title</h2>
+        <div  id="tabla1" class="table-responsive tabla visible" style="display:none;">
+            <table class="table table-striped">
+                <thead>
+                    <tr id='columna-cabeza1'>
+                        <th id="reporte">Reportes por tipos de datos</th>
+                        <th>Descripcion</th>
+                        <th>Numero de reportes totales</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr >
+                        <td id="principal">DATOS AGREGADOS TOTALES DE LA UNIVERSIDAD</td>
+                        <td>   </td>
+                    </tr>
+                    <tr>  
+                        <td id="principal2">DATOS POR UNIDAD ACADÉMICA</td>
+                    </tr>
+                    <tr>
+                        <td id="principal3">DATOS DESAGREGADOS POR UNIDAD ACADÉMICA</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div id="tabla2" class="table-responsive" style="display:none;">
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Reportes</th>
+                        <th>Descripcion</th>
+                        <th>Tipo de grafico</th>
+                        <th>Header</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Cantidad y tipo de docente</td>
+                        <td>ipsum</td>
+                        <td>dolor</td>
+                        <td>sit</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>amet</td>
+                        <td>consectetur</td>
+                        <td>adipiscing</td>
+                        <td>elit</td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td>Integer</td>
+                        <td>nec</td>
+                        <td>odio</td>
+                        <td>Praesent</td>
+                    </tr>
+                    <tr>
+                        <td id='volver'>Volver</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div  id="tabla3" class="table-responsive" style="display:none;">
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Reportes</th>
+                        <th>Descripcion</th>
+                        <th>Tipo de grafico</th>
+                        <th>Header</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>araucano y guarani</td>
+                        <td>ipsum</td>
+                        <td>dolor</td>
+                        <td>sit</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>amet</td>
+                        <td>consectetur</td>
+                        <td>adipiscing</td>
+                        <td>elit</td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td>Integer</td>
+                        <td>nec</td>
+                        <td>odio</td>
+                        <td>Praesent</td>
+                    </tr>
+                    <tr>
+                        <td id='volver2'>Volver</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div  id="tabla4" class="table-responsive" style="display:none;">
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Reportes</th>
+                        <th>Descripcion</th>
+                        <th>Tipo de grafico</th>
+                        <th>Header</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>araucano y guarani</td>
+                        <td>ipsum</td>
+                        <td>dolor</td>
+                        <td>sit</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>amet</td>
+                        <td>consectetur</td>
+                        <td>adipiscing</td>
+                        <td>elit</td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td>Integer</td>
+                        <td>nec</td>
+                        <td>odio</td>
+                        <td>Praesent</td>
+                    </tr>
+                    <tr>
+                        <td id='volver3'>Volver</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+<script src='js/botones.js'></script>
+<script src='js/funciones.js'></script>
---
+++ funciones/funciones_ajax.php
@@ -0,0 +1,7 @@
+<?php
+
+if ($_POST[id] == 'p_base')
+    include_once '../content/p_base.php';
+if ($_POST[id] == 'p_mapa')
+    include_once '../content/p_mapa.php';
+?>
